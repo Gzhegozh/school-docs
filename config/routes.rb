a9dc60c9f416160e635b624739b92d0ea1177f68
Wepoo <yurkovetc@gmail.com>
2016-03-22 15:12:28 +0300

    Add scaffold for messages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {registrations: 'user/registrations', 
                                       sessions: 'user/sessions',
                                       passwords: 'user/passwords'}
+  resources :messages
 end
