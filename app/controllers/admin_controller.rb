634c00fca90bc8899a6ef59e4b4b8c88bb1e2faf
Gzhegozh <paulivanov95@gmail.com>
2016-03-22 13:21:12 +0300

    Updating admin on school_group

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,10 @@ class AdminController < ApplicationController
   def set_admin
     @user = User.find(params[:user][:id])
     @school_group = SchoolGroup.find(params[:user][:school_group_id])
+    prev_admin = User.with_role(:admin, @school_group).first
+    prev_admin.remove_role :admin, @school_group
     @user.add_role(:admin, @school_group)
+    flash[:success] = 'Successfully changed'
     redirect_to :back
   end
 end
