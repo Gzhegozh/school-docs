ba359c96aa24f88170450dd5747e7a1d4522579a
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-03-23 21:11:56 +0300

    Some fixes

--- app/models/impersonation.rb
+++ app/models/impersonation.rb
@@ -1,4 +1,4 @@
 class Impersonation < ActiveRecord::Base
   belongs_to :user
-  belongs_to :impersonator, class: 'User'
+  belongs_to :impersonator, class_name: 'User'
 end
--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,4 @@
 class Relationship < ActiveRecord::Base
-  belongs_to :parent, class: 'User'
-  belongs_to :child, class: 'User'
+  belongs_to :parent, class_name: 'User'
+  belongs_to :child, class_name: 'User'
 end
