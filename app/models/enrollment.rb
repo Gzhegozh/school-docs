a894b5bf557064bbbce996844ff07f89e47eb8ba
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-03-23 19:42:37 +0300

    Remove some files

--- app/models/enrollment.rb
+++
@@ -1,5 +0,0 @@
-class Enrollment < ActiveRecord::Base
-  has_many :tab_instances
-  belongs_to :user
-  belongs_to :grade
-end
--- db/migrate/20160321202244_create_tab_instances.rb
+++
@@ -1,9 +0,0 @@
-class CreateTabInstances < ActiveRecord::Migration
-  def change
-    create_table :tab_instances do |t|
-      t.references :enrollment
-      t.references :tab
-      t.timestamps null: false
-    end
-  end
-end
