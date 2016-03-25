98c9d31454939a8f5f7cf17213355d8071db4085
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-03-25 23:56:59 +0300

    New features at enrollment form instance creating

--- app/assets/javascripts/components/BasicsPage.js.jsx
+++ app/assets/javascripts/components/BasicsPage.js.jsx
@@ -5,25 +5,45 @@ class BasicsPage extends React.Component {
 
     render(){
         return (
-          <div className="row">
-              <div className="col-lg-2">
-                  <p> Enter name: </p>
+          <div>
+              <h3>This is basic info about enrolling student</h3>
+              <hr/>
+              <div className="row">
+                  <div className="form-group">
+                      <label className="col-md-2 control-label"> Enter name: </label>
+                      <div className="col-md-10">
+                          <input className="form-control" type="text" ref={ (ref) => this.name = ref }/>
+                      </div>
+                  </div>
               </div>
-              <div className="col-lg-10">
-                  <input className="form-control" type="text" ref={ (ref) => this.name = ref }/>
+              <br/>
+              <div className="row">
+                  <div className="form-group">
+                      <label className="col-md-2 control-label"> Enter last name: </label>
+                      <div className="col-md-10">
+                          <input className="form-control" type="text" ref={ (ref) => this.last_name = ref }/>
+                      </div>
+                  </div>
               </div>
-              <div className="col-lg-2">
-                  <p> Enter last name: </p>
+              <br/>
+              <div className="row">
+                  <div className="form-group">
+                      <label className="col-md-2 control-label"> Enter middle name: </label>
+                      <div className="col-md-10">
+                          <input className="form-control" type="text" ref={ (ref) => this.middle_name = ref }/>
+                      </div>
+                  </div>
               </div>
-              <div className="col-lg-10">
-                  <input className="form-control" type="text" ref={ (ref) => this.last_name = ref }/>
+              <br/>
+              <div className="row">
+                  <div className="form-group">
+                      <label className="col-md-2 control-label">Enter birthday: </label>
+                      <div className="col-md-10">
+                          <input className="form-control" type="date" ref={ (ref) => this.birthday = ref }/>
+                      </div>
+                  </div>
               </div>
-              <div className="col-lg-2">
-                  <p> Enter middle name: </p>
-              </div>
-              <div className="col-lg-10">
-                  <input className="form-control" type="text" ref={ (ref) => this.middle_name = ref }/>
-              </div>
+              <br/>
           </div>
         );
     }
---
+++ app/assets/javascripts/components/ErrorPage.js.jsx
@@ -0,0 +1,14 @@
+class ErrorPage extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div>
+                <h3>Some error occured</h3>
+                <h4>Please, choose another section</h4>
+            </div>
+        );
+    }
+}
