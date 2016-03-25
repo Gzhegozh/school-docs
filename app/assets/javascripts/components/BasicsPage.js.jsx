78affc0123171d6710a36c541e0956e132edb88c
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-03-25 11:57:49 +0300

    Basic page

--- app/assets/javascripts/components/BasicsPage.js.jsx
+++ app/assets/javascripts/components/BasicsPage.js.jsx
@@ -5,7 +5,18 @@ class BasicsPage extends React.Component {
 
     render(){
         return (
-          <div>
+          <div className="row">
+              <div className="col-lg-2">
+                  <p> Enter name: </p>
+                  <input className="form-control" type="text" ref={ (ref) => this.name = ref }/>
+                  <p> Enter last name: </p>
+                  <input className="form-control" type="text" ref={ (ref) => this.last_name = ref }>
+                  <p> Enter middle name: </p>
+                  <input className="form-control" type="text" ref={ (ref) => this.middle_name = ref }>
+                  <p> Enter middle name: </p>
+                  <input className="form-control" type="text" ref={ (ref) => this.middle_name = ref }>
+              </div>
+              <div className="col-lg-10">
 
           </div>
         );
