edb3f4d7a59eab1fce9ad06a387ba355aef1d630
Aleksandr Sivakow <alexandrsivakov@me.com>
2016-03-27 23:26:13 +0300

    Added functionality of medications tab

--- app/assets/javascripts/components/Medication.js.jsx
+++ app/assets/javascripts/components/Medication.js.jsx
@@ -3,6 +3,17 @@ class Medication extends React.Component {
         super(props);
     }
 
+    valuesChanged(e){
+        e.preventDefault();
+        this.props.onUpdate({name: this.name.value,
+                             notes: this.notes.value}, this.props.index);
+    }
+
+    componentDidMount(){
+        this.name.value = this.props.medication.name;
+        this.notes.value = this.props.medication.notes;
+    }
+
     render(){
         return (
             <div className="panel panel-default panel-body">
@@ -10,12 +21,14 @@ class Medication extends React.Component {
                     <input type="text"
                            className="form-control"
                            ref={ (ref) => this.name = ref }
+                           onChange={ this.valuesChanged.bind(this) }
                            placeholder="Medication name"/>
                 </div>
                 <div className="col-lg-8">
                     <input type="text"
                            className="form-control"
                            ref={ (ref) => this.notes = ref}
+                           onChange={ this.valuesChanged.bind(this) }
                            placeholder="Notes"/>
                 </div>
             </div>
